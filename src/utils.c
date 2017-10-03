74596a5e139e44bea76f424b33d3bfd3a76617cb
chj777 <bookword@naver.com>
2017-10-03 12:09:12 +0900

    My first trial: just considering what to do.

--- src/utils.c
+++ src/utils.c
@@ -4,4 +4,21 @@ void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+	char *token = NULL;
+	char *token2 = NULL;
+	argv = (char***)malloc(sizeof(char**));
+	
+	*argc = 0;
+	
+	token = strtok(command, " ");
+
+	while(token != NULL){
+		argc++;  		
+		
+	}
+
+	printf("argc == %d\n", argc);
+
+	return;
+
 }
